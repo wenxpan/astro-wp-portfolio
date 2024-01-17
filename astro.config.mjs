ca6b404cf055417470a61aca12a1f33afd99bf9a
wenxuan-pan <panwenxuan42@gmail.com>
2024-01-18 10:42:12 +1100

    added colors theme and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro.wenxpan.com",
+  sitemap: true,
   integrations: [tailwind()]
-});
+})
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 
-const defaultTheme = require("tailwindcss/defaultTheme")
+import defaultTheme from "tailwindcss/defaultTheme"
+import colors from "tailwindcss/colors"
 
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    colors: {
+      ...colors,
+      primary: colors.cyan[700],
+      primaryDark: colors.cyan[300],
+      primarySecondary: colors.cyan[800],
+      primarySecondaryDark: colors.cyan[500]
+    },
     extend: {
       fontFamily: {
         mono: ["Fira Code Variable", ...defaultTheme.fontFamily.mono]
