5d0d75a02b12728df0b0c18bcb503e6c2920278d
wenxuan-pan <panwenxuan42@gmail.com>
2024-01-18 10:37:33 +1100

    added fira code font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
-import { defineConfig } from "astro/config"
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
-  site: "https://astro.wenxpan.com"
-})
+  site: "https://astro.wenxpan.com",
+  integrations: [tailwind()]
+});
