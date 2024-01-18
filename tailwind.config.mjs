4df2acedafe571e883ed635f550af3ad5b3517a4
wenxuan-pan <panwenxuan42@gmail.com>
2024-01-18 17:38:11 +1100

    added dark mode

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,14 +4,24 @@ import defaultTheme from "tailwindcss/defaultTheme"
 import colors from "tailwindcss/colors"
 
 export default {
+  darkMode: "class",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     colors: {
       ...colors,
       primary: colors.cyan[700],
-      primaryDark: colors.cyan[300],
-      primarySecondary: colors.cyan[800],
-      primarySecondaryDark: colors.cyan[500]
+      secondary: colors.cyan[800],
+      dark: {
+        primary: colors.cyan[300],
+        secondary: colors.cyan[500]
+      },
+      accent: {
+        gray: {
+          light: colors.gray[300],
+          dark: colors.gray[500]
+        },
+        default: colors.blue[700]
+      }
     },
     extend: {
       fontFamily: {
